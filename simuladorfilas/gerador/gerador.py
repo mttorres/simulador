f401f90b8a9cded3c372cabe21d27349b617c0cf
Deflaimun <paulohtb@hotmail.com>
2018-07-02 22:19:13 -0300

    Limpando

--- simuladorfilas/gerador/gerador.py
+++ simuladorfilas/gerador/gerador.py
@@ -1,19 +1,18 @@
 import math
 import time
-from evento.evento import Evento
 
-###### para varios valores########
-#gerador  n valores uniformes
+#       para varios valores         #
+#       gerador  n valores uniformes#
 def conglinear(a,c,m,x,vet,n):
-    for i in range(0,n):
-        xp = ((x*a) +c)%m
+    for i in range(0, n):
+        xp = ((x*a) + c) % m
         u = xp/m
         vet.append(u)
         x = xp
 
 
 # gerador exeponencial usando os valores uniformes
-def geraexp(lamb,vetuni):
+def geraexp(lamb, vetuni):
     vetexp = []
     for x in vetuni:
         res = (-(math.log(1-x)))/lamb
@@ -23,7 +22,7 @@ def geraexp(lamb,vetuni):
 
 # arredondar CASO NECESSARIO
 def geraexpdiscret(vetexp):
-    for i in range(0,len(vetexp)):
+    for i in range(0, len(vetexp)):
         vetexp[i] = round(vetexp[i]) # evita(cieling) que alguns caras cheguem no tempo zero ...
 
 
@@ -42,7 +41,6 @@ def unconglinear(a,c,m,s):
     return u
 
 def ungeraexp(lamb,u):
-    #x =unconglinear(13445,0,2**31-1,s)  # ex: antede um cliente a cada 3 minutos: rate = 1/3 a formula é ln(1-x)/1/lamb que é a mesma coisa que multiplicar
     res = (-(math.log(1 - u))) / lamb  # ex: 1/40 1 terremoto a cada 40 mins em algum lugar do mundo
     return res
 
@@ -50,88 +48,6 @@ def ungeraexp(lamb,u):
 
 
 
-
-
-
-''' TESTES
-def geradoreventos(vet,tipo,tempo):
-    evento = Evento(tipo, vet[0]+tempo)
-    vet.remove(vet[0])
-    return evento
-'''
-
-''''''
-vetuni = []
-vetuni2 =[]
-vetuni3 = []
-#s = 5
-#qtd = 33000
-conglinear(13445,0,2**31-1,5000,vetuni,3600000//110) # testando com qtd numeros e semente USAR DIFERENTES SEMENTES!
-vetuni.sort()
-#conglinear(13445,0,2**31-1,s,vetuni2,40000)
-#conglinear(13445,0,2**31-1,s,vetuni2,40000)
-expat = geraexp(1/110,vetuni) # tempos de chegada da fila atendimento
-#exppa = geraexp(1/90,vetuni) # tempos de saida da fila de atendimento e entrada da fila pagamento
-#expat.sort()
-print(max(vetuni))
-print(ungeraexp(1/110,vetuni[0]))
-print(expat)
-
-'''
-
-
-'''
-vet = []
-conglinear(13445,0,2**31-1,5,vet,1000)
-vetexp = geraexp(1/110,vet) 
-c = 0
-for x in vetexp:
-    c = c + x
-
-media = c/1000
-print(media) # como esperado proxima de 90!
-geraexpdiscret(vetexp)
-print(vetexp)
-'''
-
-#def geradoreventos()
-
-# com essas funções temos EXATAMENTE o tempo que ocorre cada evento!
-# duvida... devemos deixar esses numeros discretos? (pode atrapalhar a simulação se formos contar cada centesimo e segundo... vai ficar mt longa!)
-# porem se deixar discreta com numeros inteiros poderiamos ter valores desiguais de numeros??
-   #outra função para testar :
-
-
-# NAO UTILIZADO (TESTES)######
-#print("VETOR NUMEROS UNIFORME: ")
-#print()
-#print(vet)
-#print()
-'''
-c= 0
-
-for x in vetexp:
-    c = c + x
-
-media = c/1000
-print(media) # como esperado proxima de 40!
-
-vetexp = geraexp(1,vet)
-
-for x in vet:
-    if x > 1:
-        print("AAAAAAAAAAA")
-
-
-c = 0
-for x in vetexp:
-    if x > 1:
-        c += 1
-        print("BBBBBBBBB",c)
-        
-
-
-#print(vetexp)'''
 '''
 Distribuição exponencial prorpiedade MEMORYLESS:
     a chance de algo acontecer em um periodo de t unidades de tempo ainda é a mesma
--- simuladorfilas/main/main.py
+++ simuladorfilas/main/main.py
@@ -4,26 +4,23 @@ from padaria.cliente import Cliente
 from padaria.padaria import Padaria
 from evento.evento import Evento
 from gerador.gerador import conglinear, geraexp, unconglinear, geraexpdiscret,ungeraexp
-import time
-# OBS'S: criar uma classe padaria? que guarda informações relevantes tipo o lucro total... etc ou as filas?
+
+
 #estados: definido pelo N das filas
 # eventos: CHEGADA,SAIDA,DESISTIU(?), DE UMA FILA EM UM CERTO TEMPO
-#todo: FAZER UMA ENTRADA QUE ESCOLHA O VALOR DE EC!
-#Done.
 
 # parametros:
 # ambos sao exponenciais, valores esperado de uma variavel com distribuição exponencial = 1/lambda
 # o a fila 1 chegada a cada 110ms , lambda= 1/110 e tem processamento 90 ms, fazendo a fila 2 ter chegada 1/90
-EX = 90 #  lembrando que ele quer que usemos E[X] = 90 ms como parametro (tempo medio para servir uma requisição)(lambda = 1/90 é aproximadamente 11 se a unidade for segundos)
-#EC = 110 # lembrando que ele quer quer que usemos E[C] = 110 ms como parametro(intervalo de tempo medio entre chegadas(lambda= 1/100 é aproximadamente 9 se a unidade for segundos)
+EX = 90 # entrada Guto
+#EC = 110 Entrada guto
 EC = int(input("EC: "))
-tempomax = 3600000 # 60 MINUTOS tempo de simulação (nao é realmente necessário ser tempo real)
-# como EC pode mudar (de acordo com o enunciado do trabalho)(diferentes experimentos) seria interessante deixar generico
-# e o EC ser definido por entrada!
+tempomax = 3600000 # 60min simulacao
+
 # VETORES DE NUMEROS ALEATORIOS ENTRE 0 E 1
 vetuni = [] # vetor unitario com x numeros entre 0 e 1 # ~32 mil numeros (chegada fila 1)(lamb / 110)
 conglinear(13445,0,2**31-1,5000,vetuni,tempomax//EC)
-vetuni.sort() # ordenados...
+vetuni.sort()
 vetuni2 =[] # 40 mil numeros (saida fila 1) e chegada fila 2 (usa semente maior para fazer as saidas tenderem a serem maior que as chegadas)
 conglinear(13445,0,2**31-1,15000,vetuni2,tempomax//EX)
 vetuni2.sort()
@@ -32,25 +29,24 @@ conglinear(13445,0,2**31-1,20000,vetuni3,tempomax//85)
 vetuni3.sort()
 
 #medidas de interesse
-EWi1 = 0 # tempo medio que uma requisição permanece no sistema (somar o tempo de permanencia(saida - chegada) de todos)(fila1)
+EWi1 = 0 # tempo medio que uma requisição permanece no sistema
 EWi2 = 0 #
 EWt = 0 # EWt1 + probdesistenciaxEWt2
-ED = 0 # taxa de descarte (desistencias)?
-EDD = 0 # taxa de descarte mesmo K == 15?
+ED = 0 # taxa de descarte (desistencias)
+EDD = 0 # taxa de descarte mesmo K == 15
 EK1 = 0 # tamanho medio da fila 1
 EK2 = 0 # tamanho medio da fila 2
 utilizacaof1 = (1/EC)*EX # utilização do recurso? é simplesmente lambda*e[x] de cada fila usaremos para o grafico só
 utilizacaof2 = (1/EX)*85
+
 # lista de eventos
 # "setando o inicio" (bootstrap como ta no slide)
 padaria = Padaria()
 le = []
 evento0 = Evento(0,ungeraexp(1/EC,vetuni[0]),1) # evento de chegada inicial, na fila 1
 vetuni.remove(vetuni[0]) # remove esse tempo (nao pode chegar outro cara exatamente nesse mesmo tempo!)
-le.append(evento0) # eventos SAO ORDENADAOS POR ORDEM DE OCORRENCIA!
-# contador minutos(loop da simulação)
-timeLoop = True
-#mins = 0    # por quanto tempo devemos por ja que ele botou mili segundos!?
+le.append(evento0) # eventos SAO ORDENADAOS POR ORDEM DE OCORRENCIA
+timeLoop = True # contador minutos(loop da simulação)
 mins = 0 # tempo decorrido ate agora
 N = 0  # total de pessoas na padaria (todas as filas)
 NC = 0 # total de chegadas (1a fila) ( e consequentemente ao sistema)
@@ -58,7 +54,7 @@ NS = 0 # total de saidas  fila 1 # pode desistir dps da fila 1
 NC2 = 0 # total de chegadas a fila 2 (nao desistiu)
 NS2 = 0 # total saidas fila 2
 NSS = 0 # numero de saidas do sistema
-# os ns de cada fila podem ser checados pelo atributo ou por um metodo e sao sempre alterados dentro do loop
+
 
 
 # loop da simulação
@@ -79,17 +75,16 @@ while timeLoop:
         print("tempo medio que uma requisição permanece em f2",EWi2 / (NC2 - ED))  # tempo de cada requisição menos as desistencias
 
         break
-    #time.sleep(1)
     if (len(le) > 0 ): # tem evento para acontecer
         if (le[0].tipo == 0 and (mins <= le[0].tempo <= mins+1)):  # é um evento de chegada
-            print("cheguei!")
+            print("Chegou um evento!")
             print("tempo",le[0].tempo)
             c = Cliente()
             if (padaria.pessoasemfila(1) < K):
                 N += 1
                 NC+= 1
                 padaria.FA1.append(c) # jogou na fila 1
-                #gerar proxima chegada(outro cara) e a saida desse que chegou agora(que é tmb a chegada na fila 2)
+                #gerar proxima chegada e a saida desse que chegou agora
                 eventoch = Evento(0, ungeraexp(1 / EC, vetuni[0]), 1)
                 eventosa = Evento(1, ungeraexp(1 / EX, vetuni2[0])+le[0].tempo, 1) # salva o tempo de saida(chegada + o tempo gerado)
                 eventosa.setacheg(le[0].tempo) # salva o tempo de chegada e temos agora o intervalo (saida - chegada)
@@ -97,8 +92,8 @@ while timeLoop:
                 vetuni2.remove(vetuni2[0])
                 le.append(eventoch)
                 le.append(eventosa)
-                # e adicionar a lista de eventos
-                le.sort(key = lambda evento: evento.tempo) # ordenada a lista de eventos
+                #adicionar a lista de eventos
+                le.sort(key=lambda evento: evento.tempo) # ordenada a lista de eventos
             else:
                 print("descarte")
                 EDD = EDD + 1 # contando os descartes e dps fazer a media no final do programa EDD/total de chegadas
@@ -109,15 +104,14 @@ while timeLoop:
                                                                                                     # esse evento acontece efetivamente após chegada + saida
         elif(le[0].tipo == 1 and le[0].fila == 1 and (mins <= le[0].tempo <= mins+1)): # evento de saida da fila 1 (e chegada na fila2)
              print("saida fila 1!")
-             #N-= 1 NA VDD ELE CONTINUA NO SISTEMA
              NS+=1 # saida da fila 1
              EWi1 += le[0].tempo - le[0].cheg # (saida - chegada) (intervalo efetivo) de cada elemento na fila 1
-             padaria.deveriareceber += 9.50 # deveria receber um valor de +9.50$
+             padaria.deveriareceber += 1.00 # deveria receber um valor de +1.00$
              if (padaria.pessoasemfila(2) < K and padaria.pessoasemfila(1) > 0):
                 # processar o evento de saida da fila 1!
                 NC2 += 1
                 cliente = padaria.FA1.popleft() # devolve o cara que saiu dessa fila... # e que deve entrar na segunda fila!
-                cliente.a_pagar = 9.50 # um valor qualquer para pagar
+                cliente.a_pagar = 1.50 # um valor qualquer para pagar
                 padaria.FP1.append(cliente)
                 # gerar a saida desse cara da fila 2
                 eventosa = Evento(1, ungeraexp(1/85, vetuni3[0])+le[0].tempo, 2)
@@ -127,10 +121,8 @@ while timeLoop:
                 le.sort(key=lambda evento: evento.tempo)
              else:
                 print("desistiu") # ou descarte da fila 2
-                #EDD = EDD+1 # descarte
-                ED = ED+1 # "calote" PREJUIZO!(descarte da fila 2)
+                ED = ED+1 # "calote" (descarte da fila 2)
 
-             #mins += le[0].tempo - le[0].cheg # atualiza o minuto com o intervalo de tempo até o evento de processamento e mudança de fila acontecer efetivamente
              le.remove(le[0])  # removendo o evento atual
           #   print("minutos: ", mins)
 
@@ -146,7 +138,6 @@ while timeLoop:
             else:
                 print("isso nao deveria acontecer")
                 print("ele nao deveria tentar remover alguem da fila 2 sem ter ninguem!?")
-            #mins += le[0].tempo - le[0].cheg
             le.remove(le[0])  # removendo o evento atual
            # print("minutos: ", mins)
 
@@ -154,5 +145,3 @@ while timeLoop:
         mins = tempomax # nao tem evento na lista
         print(mins)
         # print("minutos: ", mins)
-
-# OBS pf observem se eu nao esqueci de atualizar algum valor de interesse no loop...

41f1ec12b91e239de493572862806b0de7b2533b
Deflaimun <paulohtb@hotmail.com>
2018-07-02 23:10:26 -0300

    Polindo

--- simuladorfilas/main/main.py
+++ simuladorfilas/main/main.py
@@ -10,18 +10,17 @@ from gerador.gerador import conglinear, geraexp, unconglinear, geraexpdiscret,un
 # eventos: CHEGADA,SAIDA,DESISTIU, DE UMA FILA EM UM CERTO TEMPO
 
 # parametros:
-# ambos sao exponenciais, valores esperado de uma variavel com distribuição exponencial = 1/lambda
-# o a fila 1 chegada a cada 110ms , lambda= 1/110 e tem processamento 90 ms, fazendo a fila 2 ter chegada 1/90
-EX = 90 # entrada Guto
-#EC = 110 Entrada guto
-EC = int(input("EC: "))
-tempomax = 3600000 # 60min simulacao
+# o a fila 1 chegada a cada 110ms, processamento 90 ms, fazendo a fila 2 ter chegada 90ms
+EC = 110
+EX = 90
+#EC = int(input("EC: "))   experimento
+tempomax = 3600000 #60min simulacao
 
 # VETORES DE NUMEROS ALEATORIOS ENTRE 0 E 1
-vetuni = [] # vetor unitario com x numeros entre 0 e 1 # ~32 mil numeros (chegada fila 1)(lamb / 110)
+vetuni = [] # vetor unitario com x numeros entre 0 e 1 # ~32 mil numeros (chegada fila 1)
 conglinear(13445,0,2**31-1,5000,vetuni,tempomax//EC)
 vetuni.sort()
-vetuni2 =[] # 40 mil numeros (saida fila 1) e chegada fila 2 (usa semente maior para fazer as saidas tenderem a serem maior que as chegadas)
+vetuni2 =[] # 40 mil numeros (saida fila 1) e chegada fila 2
 conglinear(13445,0,2**31-1,15000,vetuni2,tempomax//EX)
 vetuni2.sort()
 vetuni3 = [] # ~42 mil numeros (saida fila 2)
@@ -30,7 +29,7 @@ vetuni3.sort()
 
 #medidas de interesse
 EWi1 = 0 # tempo medio que uma requisição permanece no sistema
-EWi2 = 0 #
+EWi2 = 0 
 EWt = 0 # EWt1 + probdesistenciaxEWt2
 ED = 0 # taxa de descarte (desistencias)
 EDD = 0 # taxa de descarte mesmo K == 15
@@ -42,12 +41,12 @@ utilizacaof2 = (1/EX)*85
 # lista de eventos
 # "setando o inicio" (bootstrap como ta no slide)
 padaria = Padaria()
-le = []
+listaEventos = []
 evento0 = Evento(0,ungeraexp(1/EC,vetuni[0]),1) # evento de chegada inicial, na fila 1
 vetuni.remove(vetuni[0]) # remove esse tempo (nao pode chegar outro cara exatamente nesse mesmo tempo!)
-le.append(evento0) # eventos SAO ORDENADAOS POR ORDEM DE OCORRENCIA
+listaEventos.append(evento0) # eventos SAO ORDENADAOS POR ORDEM DE OCORRENCIA
 timeLoop = True # contador minutos(loop da simulação)
-mins = 0 # tempo decorrido ate agora
+tempoDecorrido = 0 # tempo decorrido ate agora
 N = 0  # total de pessoas na padaria (todas as filas)
 NC = 0 # total de chegadas (1a fila) ( e consequentemente ao sistema)
 NS = 0 # total de saidas  fila 1 # pode desistir dps da fila 1
@@ -60,52 +59,42 @@ NSS = 0 # numero de saidas do sistema
 # loop da simulação
 K = 15
 while timeLoop:
-    #print("minutos: ",mins)
-    if (mins >= tempomax):
-        print("simulação encerrada!")
-        print(le)
-        print("total de chegadas a padaria/fila1: ",NC)
-        print("total de saidas da fila1", NS)
-        print("total de chegadas a fila da fila2", NS2)
-        print("total de saidas da padaria: ", NSS)
-        print("total de descarte em f1: ", EDD)
-        print("total de CALOTES em f2: ", ED)
-        print("Prejuizo: ", padaria.recebeu - padaria.deveriareceber)
-        print("tempo medio que uma requisição permanece em f1",EWi1/(NC-EDD)) # tempo de cada requisição menos as descartadas
-        print("tempo medio que uma requisição permanece em f2",EWi2 / (NC2 - ED))  # tempo de cada requisição menos as desistencias
-
+    #print("minutos: ",tempoDecorrido)
+    if (tempoDecorrido >= tempomax):
+        timeLoop = False
         break
-    if (len(le) > 0 ): # tem evento para acontecer
-        if (le[0].tipo == 0 and (mins <= le[0].tempo <= mins+1)):  # é um evento de chegada
+
+    if (len(listaEventos) > 0 ): # tem evento para acontecer
+        if (listaEventos[0].tipo == 0 and (tempoDecorrido <= listaEventos[0].tempo <= tempoDecorrido+1)):  # é um evento de chegada
             print("Chegou um evento!")
-            print("tempo",le[0].tempo)
+            print("Tempo de chegada:",listaEventos[0].tempo)
             c = Cliente()
             N += 1
             NC+= 1
             if (padaria.pessoasemfila(1) < K):
                 padaria.FA1.append(c) # jogou na fila 1
                 #gerar proxima chegada e a saida desse que chegou agora
-                eventoch = Evento(0, ungeraexp(1 / EC, vetuni[0]), 1)
-                eventosa = Evento(1, ungeraexp(1 / EX, vetuni2[0])+le[0].tempo, 1) # salva o tempo de saida(chegada + o tempo gerado)
-                eventosa.setacheg(le[0].tempo) # salva o tempo de chegada e temos agora o intervalo (saida - chegada)
+                eventoChegada = Evento(0, ungeraexp(1 / EC, vetuni[0]), 1)
+                eventoSaida = Evento(1, ungeraexp(1 / EX, vetuni2[0])+listaEventos[0].tempo, 1) # salva o tempo de saida(chegada + o tempo gerado)
+                eventoSaida.setacheg(listaEventos[0].tempo) # salva o tempo de chegada e temos agora o intervalo (saida - chegada)
                 vetuni.remove(vetuni[0]) # remove da lista de valores 0,1 para nao ter repetições
                 vetuni2.remove(vetuni2[0])
-                le.append(eventoch)
-                le.append(eventosa)
+                listaEventos.append(eventoChegada)
+                listaEventos.append(eventoSaida)
                 #adicionar a lista de eventos
-                le.sort(key=lambda evento: evento.tempo) # ordenada a lista de eventos
+                listaEventos.sort(key=lambda evento: evento.tempo) # ordenada a lista de eventos
             else:
                 print("descarte")
                 EDD = EDD + 1 # contando os descartes e dps fazer a media no final do programa EDD/total de chegadas
-            #independente do descarte ou nao a chegada aconteceu e deve ser removida da lista e o tempo mins atualizado
-            mins = le[0].tempo
-            le.remove(le[0])  # removendo o evento atual
-         #   print("minutos: ", mins)
+            #independente do descarte ou nao a chegada aconteceu e deve ser removida da lista e o tempo tempoDecorrido atualizado
+            tempoDecorrido = listaEventos[0].tempo
+            listaEventos.remove(listaEventos[0])  # removendo o evento atual
+         #   print("minutos: ", tempoDecorrido)
                                                                                                     # esse evento acontece efetivamente após chegada + saida
-        elif(le[0].tipo == 1 and le[0].fila == 1 and (mins <= le[0].tempo <= mins+1)): # evento de saida da fila 1 (e chegada na fila2)
+        elif(listaEventos[0].tipo == 1 and listaEventos[0].fila == 1 and (tempoDecorrido <= listaEventos[0].tempo <= tempoDecorrido+1)): # evento de saida da fila 1 (e chegada na fila2)
              print("saida fila 1!")
              NS+=1 # saida da fila 1
-             EWi1 += le[0].tempo - le[0].cheg # (saida - chegada) (intervalo efetivo) de cada elemento na fila 1
+             EWi1 += listaEventos[0].tempo - listaEventos[0].cheg # (saida - chegada) (intervalo efetivo) de cada elemento na fila 1
              padaria.deveriareceber += 1.00 # deveria receber um valor de +1.00$
              if (padaria.pessoasemfila(2) < K and padaria.pessoasemfila(1) > 0):
                 # processar o evento de saida da fila 1!
@@ -114,20 +103,20 @@ while timeLoop:
                 cliente.a_pagar = 1.00 # um valor qualquer para pagar
                 padaria.FP1.append(cliente)
                 # gerar a saida desse cara da fila 2
-                eventosa = Evento(1, ungeraexp(1/85, vetuni3[0])+le[0].tempo, 2)
-                eventosa.setacheg(le[0].tempo)
+                eventoSaida = Evento(1, ungeraexp(1/85, vetuni3[0])+listaEventos[0].tempo, 2)
+                eventoSaida.setacheg(listaEventos[0].tempo)
                 vetuni3.remove(vetuni3[0])
-                le.append(eventosa)
-                le.sort(key=lambda evento: evento.tempo)
+                listaEventos.append(eventoSaida)
+                listaEventos.sort(key=lambda evento: evento.tempo)
              else:
                 print("desistiu") # ou descarte da fila 2
                 ED = ED+1 # "calote" (descarte da fila 2)
 
-             le.remove(le[0])  # removendo o evento atual
-          #   print("minutos: ", mins)
+             listaEventos.remove(listaEventos[0])  # removendo o evento atual
+          #   print("minutos: ", tempoDecorrido)
 
-        elif (le[0].tipo == 1 and le[0].fila == 2 and (mins <= le[0].tempo <= mins+1)):
-            EWi2 += le[0].tempo - le[0].cheg  # (saida - chegada) (intervalo efetivo)
+        elif (listaEventos[0].tipo == 1 and listaEventos[0].fila == 2 and (tempoDecorrido <= listaEventos[0].tempo <= tempoDecorrido+1)):
+            EWi2 += listaEventos[0].tempo - listaEventos[0].cheg  # (saida - chegada) (intervalo efetivo)
             if (padaria.pessoasemfila(2)  > 0):
                 print("saida fila 2!( e da padaria)")
                 N -= 1 # 1 saida do sistema (total de pessoas no sistema diminui)
@@ -138,10 +127,20 @@ while timeLoop:
             else:
                 print("isso nao deveria acontecer")
                 print("ele nao deveria tentar remover alguem da fila 2 sem ter ninguem!?")
-            le.remove(le[0])  # removendo o evento atual
-           # print("minutos: ", mins)
+            listaEventos.remove(listaEventos[0])  # removendo o evento atual
+           # print("minutos: ", tempoDecorrido)
 
-    elif(len(le)==0):
-        mins = tempomax # nao tem evento na lista
-        print(mins)
-        # print("minutos: ", mins)
+    elif(len(listaEventos)==0):
+        tempoDecorrido = tempomax # nao tem evento na lista
+        print("Tempo decorrido em minutos: ", tempoDecorrido/60000)
+
+print("simulação encerrada!")
+print("total de chegadas a padaria/fila1: ",NC)
+print("total de saidas da fila1", NS)
+print("total de chegadas a fila da fila2", NS2)
+print("total de saidas da padaria: ", NSS)
+print("total de descarte em f1: ", EDD)
+print("total de CALOTES em f2: ", ED)
+print("Prejuizo: ", padaria.recebeu - padaria.deveriareceber)
+print("tempo medio que uma requisição permanece em f1",EWi1/(NC-EDD)) # tempo de cada requisição menos as descartadas
+print("tempo medio que uma requisição permanece em f2",EWi2 / (NC2 - ED))  # tempo de cada requisição menos as desistencias
